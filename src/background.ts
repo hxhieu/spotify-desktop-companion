ab5885c7e36f477c67feaa572e977660a861ac07
Hugh Hoang <hugh.hoang@gmail.com>
2021-01-11 17:21:29 +1300

    chore: bump electron to 11.1

--- src/background.ts
+++ src/background.ts
@@ -57,7 +57,7 @@ app.on("ready", async () => {
   if (isDevelopment && !process.env.IS_TEST) {
     // Install Vue Devtools
     try {
-      await installExtension('ljjemllljcmogpfapbkkighbhhppjdbg');
+      await installExtension("ljjemllljcmogpfapbkkighbhhppjdbg");
     } catch (e) {
       console.error("Vue Devtools failed to install:", e.toString());
     }
