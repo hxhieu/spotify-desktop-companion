1d2616936803d2ed8c63b2d961ecb5fc7cea2cac
Hugh Hoang <hugh.hoang@gmail.com>
2021-01-12 16:34:34 +1300

    chore: lint --fix

---
+++ src/components/Toolbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="toolbar">
+    <CloseIcon />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import CloseIcon from '../assets/icons/x-circle.svg';
+
+export default defineComponent({
+  components: {
+    CloseIcon,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+@import '../_variables';
+
+#toolbar {
+  width: 100%;
+  height: 20px;
+  background: $mainDark;
+  -webkit-app-region: drag;
+}
+</style>
